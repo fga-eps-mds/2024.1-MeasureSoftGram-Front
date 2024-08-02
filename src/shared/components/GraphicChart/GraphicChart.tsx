e394faad418c513c1f4bf434f0146d39b38efbce
Christian Siqueira <chfleurysiq@gmail.com>
2024-08-02 01:46:42 -0300

    feat: add github metrics and measures graphs

--- src/shared/components/GraphicChart/GraphicChart.tsx
+++ src/shared/components/GraphicChart/GraphicChart.tsx
@@ -22,7 +22,7 @@ interface Prop {
   autoGrid?: boolean;
   addHistoricalTSQMI?: boolean;
   addCurrentGoal?: boolean;
-  metricsSource?: 'github' | 'sonarqube'
+  collectionSource?: 'github' | 'sonarqube'
 }
 
 type formatFunctionType = {
@@ -44,14 +44,14 @@ const GraphicChart = ({
   autoGrid = false,
   addHistoricalTSQMI = false,
   addCurrentGoal = false,
-  metricsSource,
+  collectionSource,
 }: Prop) => {
   const {
     data: historical,
     error,
     isLoading,
     isEmpty
-  } = useRequestValues({ type: valueType, value, addHistoricalTSQMI, addCurrentGoal, metricsSource });
+  } = useRequestValues({ type: valueType, value, addHistoricalTSQMI, addCurrentGoal, collectionSource });
   const { hasKey } = useProductConfigFilterContext();
   const [showCharts, setShowCharts] = useState(false);
   const { currentProduct } = useProductContext();
@@ -78,7 +78,7 @@ const GraphicChart = ({
     () =>
       _.range(numLines).map((i) => ({
         ...chartOption[type]({
-          historical: _.filter(sliceHistorical(i), (item) => metricsSource === 'github' || hasKey(item.key)),
+          historical: _.filter(sliceHistorical(i), (item) => collectionSource === 'github' || hasKey(item.key)),
           title: i === 0 ? title : '',
           isEmpty: isEmpty || error,
           redLimit: currentProduct?.gaugeRedLimit,
