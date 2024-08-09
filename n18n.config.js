1aec95b9204f1293e766a7124b0ff50837a86eb1
Luan-Cavalcante <luanv.cavalcante@gmail.com>
2024-08-08 23:26:16 -0300

    feat:adiciona configuração básica da internacionalização.
    Co-authored-by: Thiago Gomes <thiagosiqgomes@gmail.com>

---
+++ n18n.config.js
@@ -0,0 +1,10 @@
+const supportedLngs = ['en', 'pt'];
+
+export const ni18nConfig = {
+  fallbackLng: supportedLngs,
+  supportedLngs,
+  ns: ['translation'],
+  react: {
+    useSuspense: false
+  }
+};
