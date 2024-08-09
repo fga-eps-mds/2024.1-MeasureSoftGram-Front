58735be5306d0e4a85a6a2deefcbf94f1e69bd36
Thiago Gomes <thiagosiqgomes@gmail.com>
2024-08-09 14:31:19 -0300

    feat: add translation to full homepage
    Co-authored-by: Luan-Cavalcante <luanv.cavalcante@gmail.com>

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -17,39 +17,41 @@ import CardInfo from "./components/CardInfo/CardInfo";
 
 import ListNavCard from "./components/ListNavCard/ListNavCard";
 
+
 const Home: NextPageWithLayout = () => {
   useRequireAuth();
+  const { t } = useTranslation();
 
   const cardsData: Array<InfoData> = [
     {
-      id: "Organizações e Produtos",
+      id: t('homepage.organization.id'),
       elements: [
         {
           imageSrc: "/images/png/structure.png",
-          title: "Organizações",
-          description: "Na página de organizações, você pode encontrar uma lista de todas as organizações que estão utilizando o MeasureSoftGram, além de encontrar também todos os seus produtos cadastrados.",
+          title: t('homepage.organization.title'),
+          description: t('homepage.organization.description'),
           routeTo: 'products'
         },
         {
           imageSrc: "/images/png/development.png",
-          title: "Produtos",
-          description: "Os produtos são softwares que pertencem à alguma organização e que possuem algum cliente. Por conta de sua natureza, um mesmo produto pode possuir vários repositórios associados à ele, o que permite uma implementação continua em mais de uma frente de trabalho.",
+          title: t('homepage.product.title'),
+          description: t('homepage.product.description'),
           routeTo: 'products/create'
         }
       ]
     },
     {
-      id: "Repositórios e Releases",
+      id: t('homepage.repository.id'),
       elements: [
         {
           imageSrc: "/images/png/folders.png",
-          title: "Repositórios",
-          description: "Repositórios são espaços de armazenamento de pacotes de software. Esses pacotes podem ser acessados e instalados por qualquer um que tenha aceso à eles, porém, o repositório armazena apenas modificações de quem é autorizado para isso. Tal característica permite que um único produto possua diversos repositórios, sendo que cada um possui as próprias modificações, permitindo o desenvolvimento simultâneo de funcionalidades diferentes."
+          title: t('homepage.repository.title'),
+          description: t('homepage.repository.description')
         },
         {
           imageSrc: "/images/png/new-offer.png",
-          title: "Releases",
-          description: "Releases representam as versões de lançamentos de um produto. Um produto, sistema computacional e seus subsistemas pode ter seu código-fonte centralizado em um repositório ou distribuído em múltiplos. No caso de múltiplos repositórios cada um possui suas respectivas releases. Além disso, existem dois tipos de releases, as majors e minors. O MeasureSoftGram atua no planejamento das releases majors utilizando a pré-configuração e suas subsequentes configurações."
+          title: t('homepage.release.title'),
+          description: t('homepage.release.description')
         }
       ]
     },
@@ -70,22 +72,22 @@ const Home: NextPageWithLayout = () => {
     //   ]
     // },
     {
-      id: "Pré-configuração",
+      id: t('homepage.pre-config.id'),
       elements: [
         {
           imageSrc: "/images/png/setting.png",
-          title: "Pré-configuração",
-          description: " A Pré-configuração representa o setup do modelo. Ela traz habilitada todas as métricas, medidas e seus valores de referência, subcaracterísticas, características, calculadas pelo modelo. Também há uma pré-definição dos pesos relativos para as medidas, subcaracterísticas e características, que são distribuídos de forma proporcional às respectivas quantidades.  Ao planejar a primeira release do produto, a  pré-configuração “deixa de existir” e passam haver as configurações. É definida no arquivo msgram.json."
+          title: t('homepage.pre-config.title'),
+          description: t('homepage.pre-config.description')
         }
       ]
     },
     {
-      id: "Configuração",
+      id: t('homepage.config.id'),
       elements: [
         {
           imageSrc: "/images/png/web-management.png",
-          title: "Configuração",
-          description: "Uma configuração representa o resultado do planejamento dos objetivos de medição da qualidade de um release. Esse planejamento envolve a definição de qual(ais) características da qualidade que serão observadas durante o período de desenvolvimento de um release. Assim, é possível escolher e configurar os parâmetros das métricas, medidas e indicadores, em  todos os níveis do modelo. É possível definir valores de pesos, matriz da relação das características, valores de referência das medidas e escolha de características, sub-características, métricas e medidas a a serem observadas."
+          title: t('homepage.config.title'),
+          description: t('homepage.config.description')
         }
       ]
     }
@@ -93,12 +95,11 @@ const Home: NextPageWithLayout = () => {
 
   const navListData: Array<string> = cardsData.map(cardData => cardData.id);
 
-  const { t } = useTranslation();
 
   return (
     <>
       <Head>
-        <title> Tela inicial MeasureSoftGram </title>
+        <title>{t('homepage.title')} - MeasureSoftGram</title>
       </Head>
 
       <Container>
@@ -119,12 +120,12 @@ const Home: NextPageWithLayout = () => {
           >
             <Box display="flex">
               <Typography variant="h4" style={{ color: "#33568E", fontWeight: "bold" }}>
-                {t('homepage.title')}
+                {t('homepage.sub-title')}
               </Typography>
             </Box>
             <Box>
               <Typography style={{ fontSize: "16px" }}>
-                Aqui você poderá obter algumas informações sobre nosso produto.
+                {t('homepage.description')}
               </Typography>
             </Box>
 
