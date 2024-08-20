98f1cc8e42a3465be025278807e16843c4e87779
leonardomilv3 <leonardomilov@gmail.com>
2024-08-20 20:35:03 -0300

    feat: repository search button
    Co-authored-by: ciceroff <cicero.barrozo13@gmail.com>

--- src/pages/products/[product]/repositories/[repository]/components/GithubRepositoriesModal/GithubRepositoriesModal.tsx
+++ src/pages/products/[product]/repositories/[repository]/components/GithubRepositoriesModal/GithubRepositoriesModal.tsx
@@ -1,5 +1,7 @@
 import { Box, Typography, Modal } from '@mui/material';
 import { useState } from 'react';
+import SearchButton from '@components/SearchButton';
+import { Repository } from '@customTypes/repository';
 
 
 type GithubRepositoriesModalProps = {
@@ -16,32 +18,52 @@ const styleModal = {
   transform: 'translate(-50%, -50%)',
   width: 800,
   height: 400,
-
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
 
+
+
 export function GithubRepositoriesModal({ handleCloseModal, open }: GithubRepositoriesModalProps) {
+
+  const [filteredRepositories, setFilteredRepositories] = useState(repositories);
+
+  function handleRepositoriesFilter(name: string) {
+    if (!name) {
+      setFilteredRepositories(repositories);
+      return;
+    }
+    const repositoriesWithName =
+      repositories?.filter((currentRepository) =>
+        currentRepository.name.toLowerCase().includes(name.toLowerCase())
+      ) ?? [];
+
+    setFilteredRepositories(repositoriesWithName);
+  }
+
   return <Modal
     open={open}
     onClose={handleCloseModal}
   >
     <Box sx={{ ...styleModal, display: 'flex', flexDirection: 'column', justifyContent: 'between', alignItems: 'center' }} >
       <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ mb: 4 }}>
-        Escolha um repostório para importar a
+        Escolha um repostório para importar
       </Typography>
 
 
-      <ul style={{background: 'grey'}}>
-        {repositories.map((repository) => {
-        return <Typography id="modal-modal-description" style={{ borderWidth: 2, borderColor: 'black', width: 500 , height: 100}} key={repository.id} sx={{ mt: 2 }}>
-          {repository.name}
-        </Typography>
-      })}
+      <SearchButton
+        onInput={(e) => handleRepositoriesFilter(e.target.value)}
+        label="Insira o nome do repositório"
+      />
+      <ul style={{background: 'grey', overflow: 'scroll',}}>
+        {filteredRepositories.map((repository) => {
+          return <Typography id="modal-modal-description" style={{ borderWidth: 2, borderColor: 'black', width: 500 , height: 100}} key={repository.id} sx={{ mt: 2 }}>
+            {repository.name}
+          </Typography>
+        })}
       </ul>
-
     </Box>
   </Modal>
 }
