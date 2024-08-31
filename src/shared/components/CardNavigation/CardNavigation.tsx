613fa9bed6fcd4ee1c6c0826fe83bb4504930eb1
Christian Siqueira <chfleurysiq@gmail.com>
2024-08-31 11:59:39 -0300

    feat: new CardNavigation component

--- src/shared/components/CardNavigation/CardNavigation.tsx
+++ src/shared/components/CardNavigation/CardNavigation.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import Link from 'next/link';
 
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Typography, Divider } from '@mui/material';
 import { ChevronRight } from '@mui/icons-material';
 
-import * as Styles from './styles';
 
 interface Props {
   id: string;
   name: string;
   url: string;
+  description: string;
 }
 
-const CardNavigation: React.FC<Props> = ({ id, name, url }) => (
-  <Button key={id} variant="outlined">
-    <Link href={url}>
-      <Box display="flex" alignItems="center" padding="10px" /*<paddingX="10px" paddingY="5px"*/>
-        <Styles.Circle />
+const CardNavigation: React.FC<Props> = ({ id, name, url, description }) => (
+  <Button key={id} variant="outlined" style={{ textAlign: 'left', width: '100%', justifyContent: 'space-between' }}>
+    <Link href={url} passHref>
+      <Box display="flex" alignItems="center" justifyContent="space-between" width="100%" padding="10px">
+        <Box display="flex" flexDirection="column" width="100%">
+          <Typography variant="subtitle1" style={{ color: '#2B4D6F', fontWeight: 'bold' }}>
+            {name}
+          </Typography>
 
-        <Typography variant="subtitle1">{name}</Typography>
+          <Divider style={{ backgroundColor: 'grey', margin: '10px 0' }} />
 
-        {/*<Box display="flex" alignItems="center" marginLeft="15px">
-          <ChevronRight />
-        </Box>*/}
+          <Typography variant="body2" color="textSecondary">
+            {description}
+          </Typography>
+        </Box>
+
+        <Box
+          width="3em"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          marginLeft={"2em"}
+        >
+          <ChevronRight fontSize="large" />
+        </Box>
       </Box>
     </Link>
   </Button>
