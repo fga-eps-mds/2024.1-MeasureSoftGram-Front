f465f20d88c32b9ef1bdaea20a1eed3f1e7acd6f
brenno-silva <brenno.silva037@gmail.com>
2024-09-03 02:21:27 -0300

    fix: ajusta testes de criação de release

--- src/pages/products/[product]/releases/create/tests/ReleaseCreation.spec.tsx
+++ src/pages/products/[product]/releases/create/tests/ReleaseCreation.spec.tsx
@@ -769,4 +769,271 @@ describe('ReleaseInfo Component', () => {
       });
     });
   });
+
+  it('should test back button', async () => {
+    (productQuery.getProductDefaultPreConfig as jest.Mock).mockResolvedValue({
+      data: {
+        "characteristics": [
+          {
+            "key": "reliability",
+            "weight": 34,
+            "subcharacteristics": [
+              {
+                "key": "testing_status",
+                "weight": 50,
+                "measures": [
+                  {
+                    "key": "passed_tests",
+                    "weight": 33,
+                    "min_threshold": 0,
+                    "max_threshold": 1
+                  },
+                  {
+                    "key": "test_builds",
+                    "weight": 33,
+                    "min_threshold": 0,
+                    "max_threshold": 300000
+                  },
+                  {
+                    "key": "test_coverage",
+                    "weight": 34,
+                    "min_threshold": 60,
+                    "max_threshold": 100
+                  }
+                ]
+              },
+              {
+                "key": "maturity",
+                "weight": 50,
+                "measures": [
+                  {
+                    "key": "ci_feedback_time",
+                    "weight": 100,
+                    "min_threshold": 1,
+                    "max_threshold": 900
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "key": "maintainability",
+            "weight": 33,
+            "subcharacteristics": [
+              {
+                "key": "modifiability",
+                "weight": 100,
+                "measures": [
+                  {
+                    "key": "non_complex_file_density",
+                    "weight": 33,
+                    "min_threshold": 0,
+                    "max_threshold": 10
+                  },
+                  {
+                    "key": "commented_file_density",
+                    "weight": 33,
+                    "min_threshold": 10,
+                    "max_threshold": 30
+                  },
+                  {
+                    "key": "duplication_absense",
+                    "weight": 34,
+                    "min_threshold": 0,
+                    "max_threshold": 5
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "key": "functional_suitability",
+            "weight": 33,
+            "subcharacteristics": [
+              {
+                "key": "functional_completeness",
+                "weight": 100,
+                "measures": [
+                  {
+                    "key": "team_throughput",
+                    "weight": 100,
+                    "min_threshold": 45,
+                    "max_threshold": 100
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+    });
+
+    await act(async () => {
+      render(
+        <ReleaseCreation />);
+    });
+
+    const { t } = useTranslation('plan_release');
+
+    const releaseNameInput = screen.getByTestId('apelido-release') as HTMLInputElement;
+
+    fireEvent.change(releaseNameInput, { target: { value: "Nome Teste" } });
+
+    const checkbox = screen.getByLabelText(t('followLastConfig')) as HTMLInputElement;
+    fireEvent.click(checkbox);
+
+    const nextButton = screen.getByText(/Next/i);
+
+    await waitFor(async () => {
+      fireEvent.click(nextButton);
+      await waitFor(async () => {
+        const backButton = screen.getByText(t('back'));
+        fireEvent.click(nextButton);
+        await waitFor(async () => {
+          fireEvent.click(nextButton);
+          expect(screen.getByText(t("balanceGoal"))).toBeInTheDocument();
+          fireEvent.click(backButton);
+          fireEvent.click(backButton);
+          fireEvent.click(backButton);
+        });
+      });
+    });
+  });
+
+  it('should test checks values', async () => {
+    (productQuery.getProductDefaultPreConfig as jest.Mock).mockResolvedValue({
+      data: {
+        "characteristics": [
+          {
+            "key": "reliability",
+            "weight": 34,
+            "subcharacteristics": [
+              {
+                "key": "testing_status",
+                "weight": 50,
+                "measures": [
+                  {
+                    "key": "passed_tests",
+                    "weight": 33,
+                    "min_threshold": 0,
+                    "max_threshold": 1
+                  },
+                  {
+                    "key": "test_builds",
+                    "weight": 33,
+                    "min_threshold": 0,
+                    "max_threshold": 300000
+                  },
+                  {
+                    "key": "test_coverage",
+                    "weight": 34,
+                    "min_threshold": 60,
+                    "max_threshold": 100
+                  }
+                ]
+              },
+              {
+                "key": "maturity",
+                "weight": 50,
+                "measures": [
+                  {
+                    "key": "ci_feedback_time",
+                    "weight": 100,
+                    "min_threshold": 1,
+                    "max_threshold": 900
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "key": "maintainability",
+            "weight": 33,
+            "subcharacteristics": [
+              {
+                "key": "modifiability",
+                "weight": 100,
+                "measures": [
+                  {
+                    "key": "non_complex_file_density",
+                    "weight": 33,
+                    "min_threshold": 0,
+                    "max_threshold": 10
+                  },
+                  {
+                    "key": "commented_file_density",
+                    "weight": 33,
+                    "min_threshold": 10,
+                    "max_threshold": 30
+                  },
+                  {
+                    "key": "duplication_absense",
+                    "weight": 34,
+                    "min_threshold": 0,
+                    "max_threshold": 5
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "key": "functional_suitability",
+            "weight": 33,
+            "subcharacteristics": [
+              {
+                "key": "functional_completeness",
+                "weight": 100,
+                "measures": [
+                  {
+                    "key": "team_throughput",
+                    "weight": 100,
+                    "min_threshold": 45,
+                    "max_threshold": 100
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+    });
+
+    await act(async () => {
+      render(
+        <ReleaseCreation />);
+    });
+
+    const { t } = useTranslation('plan_release');
+
+    const releaseNameInput = screen.getByTestId('apelido-release') as HTMLInputElement;
+
+    fireEvent.change(releaseNameInput, { target: { value: "Nome Teste" } });
+
+    const checkbox = screen.getByLabelText(t('followLastConfig')) as HTMLInputElement;
+    fireEvent.click(checkbox);
+
+    const nextButton = screen.getByText(/Next/i);
+
+    await waitFor(async () => {
+      fireEvent.click(nextButton);
+      await waitFor(async () => {
+        const changeRefValues = screen.getByText(t('changeRefValues'));
+        fireEvent.click(changeRefValues);
+        await waitFor(async () => {
+          const continueBtn = screen.getByText(t('continue'));
+          fireEvent.click(continueBtn);
+          fireEvent.click(nextButton);
+          await waitFor(async () => {
+            expect(screen.getByText(t("refValues"))).toBeInTheDocument();
+            fireEvent.click(nextButton);
+            await waitFor(async () => {
+              const allowBalanceGoalBtn = screen.getByText(t('allowBalanceGoal'));
+              fireEvent.click(allowBalanceGoalBtn);
+              expect(screen.getByText(t("balanceGoal"))).toBeInTheDocument();
+            });
+          });
+        });
+      });
+    });
+  });
 });
