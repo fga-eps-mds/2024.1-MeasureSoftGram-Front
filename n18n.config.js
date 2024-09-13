ac4289458ad30744d83f1b43cf8452b14ca875d3
Thiago Gomes <thiagosiqgomes@gmail.com>
2024-09-13 01:23:16 -0300

    feat: add internationalization to header

--- n18n.config.js
+++ n18n.config.js
@@ -3,7 +3,7 @@ const supportedLngs = ['en', 'pt'];
 export const ni18nConfig = {
   fallbackLng: supportedLngs,
   supportedLngs,
-  ns: ['translation', 'home', 'about', 'sidebar', 'settings', 'releases', 'repositories', 'overview'],
+  ns: ['translation', 'home', 'about', 'sidebar', 'settings', 'releases', 'repositories', 'overview', 'header'],
   react: {
     useSuspense: false
   }
